fafb4ff8758b8285ee4e23bc2349f274ebbc0849
ArisaBonsaiTree <angelevarela@hotmail.com>
2023-02-26 18:58:51 -0800

    Get Student

--- src/com/classroster/ui/ClassRosterView.java
+++ src/com/classroster/ui/ClassRosterView.java
@@ -53,5 +53,24 @@ public class ClassRosterView {
         io.print("=== Display All Students ===");
     }
 
+    public void displayDisplayStudentBanner(){
+        io.print("=== Display Student ===");
+    }
+
+    public String getStudentIdChoice(){
+        return io.readString("Please enter the Student ID.");
+    }
+
+    public void displayStudent(Student student){
+        if(student != null){
+            io.print(student.getStudentId());
+            io.print(student.getFirstName() + " " + student.getLastName());
+            io.print("");
+        }else{
+            io.print("No such student.");
+        }
+        io.readString("Please hit enter to continue");
+    }
+
 }
 
